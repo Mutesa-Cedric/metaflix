bd9a6f50a03e61ab1f206c6e9c07746e88b1460d
Mutesa-Cedric <mutesacedrid@gmail.com>
2022-05-26 16:17:16 +0200

    fix: error fix

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,12 +1,11 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
-// import { DocumentData } from 'firebase/firestore'
 import { useRef, useState } from 'react'
 import { Movie } from '../typings'
 import Thumbnail from './Thumbnail'
 
 interface Props {
   title: string
-  movies: Movie[] | DocumentData[]
+  movies: Movie[]
 }
 
 function Row({ title, movies }: Props) {

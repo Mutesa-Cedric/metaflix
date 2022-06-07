e3d38c7a4c1b62b78cad4c4b130982baf1d9fb92
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-07 11:28:29 +0200

    added FAQ list

---
+++ constants/Questions.ts
@@ -0,0 +1,31 @@
+
+
+export const questions: object[] = [
+    {
+        q: "What is Metaflix ?",
+        a: `Metflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.
+        You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!`
+    },
+    {
+        q: "How much does Metaflix Cost ?",
+        a: `Watch Metflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from US$3.99 to US$11.99 a month. No extra costs, no contracts.`
+    },
+    {
+        q: "Where can I watch ?",
+        a: `Watch anywhere, anytime. Sign in with your Metflix account to watch instantly on the web at Metflix.com from your personal computer or on any internet-connected device that offers the Metflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+        You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Metflix with you anywhere.`
+    },
+    {
+        q: "How do I quit ?",
+        a: `Metflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`
+    },
+    {
+        q: "What can I watch on Metflix ?",
+        a: `Metflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Metflix originals, and more. Watch as much as you want, anytime you want.`
+    },
+    {
+        q: "Is Metaflix good for Kids ?",
+        a: `The Metflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.
+        Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.`
+    }
+]

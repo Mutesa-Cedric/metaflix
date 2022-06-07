567b1e66a93ffd04f5f8fc0ed928785f337c8d90
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-07 07:48:17 +0200

    feature: second section of welcome page

--- pages/Welcome.tsx
+++ pages/Welcome.tsx
@@ -10,7 +10,7 @@ function Welcome() {
                 <title>Metaflix -  Welcome To Metaflix</title>
                 <link rel="icon" href="/mlogo.png" />
             </Head>
-            <main className="relative ">
+            <main className="relative bg-black">
                 <div className=" bg-cover  bg-no-repeat h-[98vh] w-screen border-b-[8px] border-[#222]"
                     style={{ backgroundImage: `linear-gradient(to top,rgba(0,0,0,.9) 0,rgba(0,0,0,.2) 60%,rgba(0,0,0,.3) 70%,rgba(0,0,0,.8) 100%),url(/banner.jpg)` }}>
                     <div className="flex justify-between items-center px-12">
@@ -30,11 +30,30 @@ function Welcome() {
                             <button className="w-[45%] text-3xl bg-[#e50914] font-medium rounded-r-sm flex items-center justify-evenly"><span>Get Started</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-8" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                                </svg>      
+                                </svg>
                             </button>
                         </div>
                     </div>
                 </div>
+                {/* second section */}
+                <div className="px-52 flex justify-between border-b-[8px] border-[#222] py-24 items-center">
+                    <div className="">
+                        <h1 className="text-5xl pb-8 font-medium">Enjoy on your TV.</h1>
+                        <p className="text-2xl flex w-3/4">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
+                    </div>
+                    <div className="relative">
+                        <Image src={'/tv.png'} objectFit="cover"
+                            width={700} height={420} />
+                        <video autoPlay loop style={{ width: '398px', height: '190px' }} className="absolute top-[40px] left-10">
+                            <source src="/video-tv-0819.m4v" />
+                        </video>
+                    </div>
+                </div>
+                {/* second section */}
+                {/* third section */}
+
+                
+                {/* third section */}
             </main>
         </div>
     )

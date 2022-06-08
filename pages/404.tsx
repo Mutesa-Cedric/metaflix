b8a0f171e8325400b5d5ee3043915a22b51dfd46
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-08 18:39:45 +0200

    fix: try to fix deployment error

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,15 +11,15 @@ function NotFund() {
                 <link rel="icon" href="/mlogo.png" />
             </Head>
             <header className='w-full bg-black py-3 pl-8'>
-                <Link href={'/'}>
-                    <Image src="logo.png" alt="logo" className='w-32 h-14' />
+                <Link href={'/Welcome'}>
+                    <img src="logo.png" alt="logo" className='w-32 h-14' />
                 </Link>
             </header>
             <main className='bg-no-repeat bg-center bg-cover w-screen h-[89vh] relative' style={{ backgroundImage: `linear-gradient(rgba(0,0,0,.1), rgba(0,0,0,.2)), url(/bg-lost-in-space.png)` }}>
                 <div className='w-full h-full flex flex-col justify-center items-center'>
                     <h1 className='md:text-7xl text-3xl font-bold mb-6 mx-auto'>Lost your way?</h1>
                     <p className='md:text-3xl text-xl w-3/5 text-center my-4 font-[400]'>Sorry, we can't find that page.You'll find lots to explore on home page.</p>
-                    <Link href={'/'}>
+                    <Link href={'/Welcome'}>
                         <button className='text-black bg-white md:px-8 px-4 md:py-2 py-1 my-5 rounded-md text-[18px] font-medium'>
                             Metaflix Home
                         </button>

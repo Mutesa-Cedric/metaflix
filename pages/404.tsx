f7e28bf070fef1ce91cce12e81170df22c4f555d
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-08 21:09:57 +0200

    fix: fixed deployment bug

--- pages/404.tsx
+++ pages/404.tsx
@@ -17,12 +17,12 @@ function NotFund() {
             </header>
             <main className='bg-no-repeat bg-center bg-cover w-screen h-[89vh] relative' style={{ backgroundImage: `linear-gradient(rgba(0,0,0,.1), rgba(0,0,0,.2)), url(/bg-lost-in-space.png)` }}>
                 <div className="w-full h-full flex flex-col justify-center items-center">
-                    {/* <h1 className="md:text-7xl text-3xl font-bold mb-6 mx-auto">
+                    <h1 className="md:text-7xl text-3xl font-bold mb-6 mx-auto">
                         Lost your way?
                     </h1>
                     <p className="md:text-3xl text-xl w-3/5 text-center my-4 font-[400]">
-                        Sorry, we can't find that page.You'll find lots to explore on home page.
-                    </p> */}
+                        Sorry, we can't find that page.You will find lots to explore on home page.
+                    </p>
                     <Link href={"/Welcome"}>
                         <button className="text-black bg-white md:px-8 px-4 md:py-2 py-1 my-5 rounded-md text-[18px] font-medium">
                             Metaflix Home

88051cef028060dc337d9eac80c0c250bb66d1a4
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-11 20:32:37 +0200

    feature : finished login and logout with firebase

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -17,34 +17,66 @@ import { auth } from '../firebase'
 
 //this is my custom hook that will be used to authenticate the user
 function useAuth() {
-
     // loading
     const [loading, setLoading] = useState(false)
 
     // loading
 
     const [user, setUser] = useState<User | null>(null);
-    const router=useRouter();
+    const router = useRouter();
     // signup function
-    const signup=async (email:string,password:string) => {
+    const signUp = async (email: string, password: string) => {
         setLoading(true);
 
         // creating a use with email and password
         //this is a function from firebase that will create a user with email and password
         //it receives auth,email and password as parameters
 
-        await createUserWithEmailAndPassword(auth,email,password)
-        .then((userCredential) => {
-            setUser(userCredential.user);
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setUser(userCredential.user);
+                router.push('/');
+                setLoading(false)
+            }).catch((err) => {
+                alert(err.message);
+            }).finally(() => {
+                setLoading(false);
+            })
+    }
+    // signup function
+
+    // signin function
+    const signIn = async (email: string, password: string) => {
+        setLoading(true);
+        await signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setLoading(false)
+                setUser(userCredential.user);
+                router.push('/');
+            }).catch((err) => {
+                alert(err.message);
+            }).finally(() => {
+                setLoading(false);
+            })
+    }
+    //signin function
+
+    //logout function
+
+    const logout=async()=>{
+        setLoading(true);
+        await signOut(auth).then(()=>{
+            setLoading(false);
+            setUser(null);
             router.push('/');
-            setLoading(false)
         }).catch((err)=>{
             alert(err.message);
         }).finally(()=>{
             setLoading(false);
-        })
+        })   
     }
-    // signup function
+
+    //logout function
 
     return user;
 }

96b7f78c45fbf3b2658e513df7ded15d9ccc9d8c
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-11 20:17:34 +0200

    feature : finished signup function

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,4 +1,5 @@
 
+// imports
 import {
     createUserWithEmailAndPassword,
     onAuthStateChanged,
@@ -11,10 +12,39 @@ import { useRouter } from 'next/router'
 import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { auth } from '../firebase'
 
+// imports
+
 
 //this is my custom hook that will be used to authenticate the user
 function useAuth() {
-    const [user, setuser] = useState();
+
+    // loading
+    const [loading, setLoading] = useState(false)
+
+    // loading
+
+    const [user, setUser] = useState<User | null>(null);
+    const router=useRouter();
+    // signup function
+    const signup=async (email:string,password:string) => {
+        setLoading(true);
+
+        // creating a use with email and password
+        //this is a function from firebase that will create a user with email and password
+        //it receives auth,email and password as parameters
+
+        await createUserWithEmailAndPassword(auth,email,password)
+        .then((userCredential) => {
+            setUser(userCredential.user);
+            router.push('/');
+            setLoading(false)
+        }).catch((err)=>{
+            alert(err.message);
+        }).finally(()=>{
+            setLoading(false);
+        })
+    }
+    // signup function
 
     return user;
 }

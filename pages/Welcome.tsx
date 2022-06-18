f263ccc7dadea63e42e5b989b2e45afcc47bb9c8
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-18 16:01:10 +0200

    fix : welcome page bug

--- pages/Welcome.tsx
+++ pages/Welcome.tsx
@@ -124,6 +124,8 @@ function Welcome() {
                                 </svg>
                             </button>
                         </div>
+
+                        
                     </div>
                 </div>
 
